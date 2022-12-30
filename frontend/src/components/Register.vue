f0e78d98698640230a0d58508d3cc9ce55a2b318
Sara Moreira <morirasara340@gmail.com>
2022-12-30 14:23:17 -0300

    Add frontend

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+<h1>Register</h1>
+
+<input
+type="text" 
+name="email" 
+v-model="email"
+placeholder="email"
+>
+<br>
+<input 
+type="password" 
+name="password" 
+placeholder="password" 
+v-model="password">
+<br>
+
+<button @click="register"> Register</button>
+  </div>
+</template>
+
+<script>
+import Authentication from '@/services/Authentication'
+export default {
+  data () {
+    return {
+      email: null,
+      password: null
+
+    }
+  },
+
+  methods: {
+   async register () {
+     await Authentication.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log('clicked register')
+    }
+    
+  },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Register />
+  </div>
+</template>
+
+<script>
+import Register from '@/components/Register.vue'
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    Register
+  }
+}
+</script>
+   
